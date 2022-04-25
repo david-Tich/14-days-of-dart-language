e8491bb2c65c133411c0f95d77360f1deb3bd22a
dave <davidkiptoo2019@gmail.com>
2022-04-25 20:57:43 +0300

    first commit

---
+++ project_one/bin/1/statements_exp_operators.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  //statements
+  print('hello world');
+
+  //Expretions===========================
+  "hello world";
+
+  4 * 6;
+  6 / 7;
+
+  //Arithmetic operations=============================
+
+  print(5 / 7);
+  print(678 * 88);
+  print(56 + 67);
+  print(56 - 78);
+
+  //Integer division=======================================
+  print(70 ~/ 9);
+
+//variables=============================================
+}
---
+++ project_one/bin/1/variables_constants.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  int number = 7;
+  int result = 2 * 7;
+  int result2 = number + result;
+
+  print(number);
+  print(result);
+  print(result2);
+}
